db545a7a233235fd8207354f7b66516bc3300d0a
Matthias Naumann <mathew96@endofnet.org>
2019-10-20 15:11:02 +0200

    Added bumperScore increase

---
+++ Binball/Assets/Scripts/BumperScore.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperScore : MonoBehaviour
+{
+    [Tooltip("how many points a hit on this object gets")]
+    public int pointsPerHit = 1;
+
+    private ScoreBoard scoreBoard;
+
+    private void Start()
+    {
+        // get scoreboard object
+        GameObject obj = GameObject.Find("scoreText");
+        if (obj != null)
+        {
+            scoreBoard = obj.GetComponent<ScoreBoard>();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if a ball collided with this it has a ballController
+        if(collision.gameObject.GetComponent<BallController>() != null)
+        {
+            scoreBoard.gamescore += pointsPerHit;
+        }
+    }
+}
