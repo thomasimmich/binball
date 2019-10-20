072d355c46b11d387a2bccf78e91a2a3bb402c36
thomas.immich <thomas.immich@centigrade.de>
2019-10-20 16:33:04 +0200

    Added gravity field to game controller

--- Binball/Assets/GameController.cs
+++ Binball/Assets/GameController.cs
@@ -2,24 +2,27 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameController : MonoBehaviour
-{
-    private void Update()
-    {
+public class GameController : MonoBehaviour {
+
+    public float gravity = 9.81f;
+
+    public void Start() {
+        Physics2D.gravity = new Vector2(0, -gravity);
+    }
+    private void Update() {
         // Check reset button
-        if(Input.GetKey(KeyCode.R))
-        {
+        if (Input.GetKey(KeyCode.R)) {
             ResetGame();
         }
+
     }
 
-    private void ResetGame()
-    {
+    private void ResetGame() {
         // get scoreboard object
         ScoreBoard scoreBoard;
+        
         GameObject obj = GameObject.Find("scoreText");
-        if (obj != null)
-        {
+        if (obj != null) {
             scoreBoard = obj.GetComponent<ScoreBoard>();
             scoreBoard.gamescore = 0;
         }
